5f3313b87e1bda16f5535180ae0baae489e0acc8
jonnhy123 <jhonatan_mattana@outlook.com>
2021-11-09 22:16:45 -0300

    Perfil de desenvolvimento

---
+++ src/main/java/com/main/helpdesk/config/DevConfig.java
@@ -0,0 +1,35 @@
+package com.main.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.main.helpdesk.services.DBService;
+
+/**
+ * Create class by: @author Jhonatan
+ *
+ *  8 de nov. de 2021 22:42:14 
+ */
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	@Bean
+	public boolean instanciaDB() {
+		if (value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		
+		return false;
+	}
+	
+}
